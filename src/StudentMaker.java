6e3549f932c529edc9b2bab60f51dc0747100994
michael <michaelk2026@student.cis.edu.hk>
2024-02-06 11:01:51 +0800

    df

--- src/StudentMaker.java
+++ src/StudentMaker.java
@@ -1,8 +1,11 @@
 public class StudentMaker {
-  
   public static void main(String[] args) {
-    
-    
+    Address address = new Address("Causeway Bay", "Tung Lo Wan Road", 180);
+    Student student = new Student("Belinda", "Yu", 17, 12, address);
+    student.setFirstName("Bella");
+    System.out.println(student.getAge());
+    int age = student.birthday();
+    System.out.println(age);
+    System.out.println(student);
   }
-  
 }
